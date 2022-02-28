9421f35c2ab20d6d89758e1f649bdc1d3b6d74ed
Max Cyprys <mc.cyprysik@icloud.com>
2022-03-01 00:03:54 +0100

    site

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template
+import requests
+import sqlite3
+
+nanopool_id = '0x4fcca869dd1dbadd57dfbbcb317410e39a0b0745'
+worker_id = 'Nikaragua'
+
+app = Flask(__name__)
+
+'''The function takes the value of an account on a nanopool
+and returns its contents in the form of the quantity ETH'''
+
+
+def balance_eth() -> str:
+    link = f'https://api.nanopool.org/v1/eth/balance/{nanopool_id}'
+    response = str(requests.get(link).json()['data'])
+    return response
+
+
+'''️Function retrieves current ETH value in USD from API
+www.cryptocompare.com'''
+
+
+def eth_currency_price_usd() -> str:
+    link = 'https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD'
+    response = requests.get(link).json()['USD']
+    return response
+
+
+'''️The function gets the value of the account on the nanopool
+and returns its contents in the form of the number of USD'''
+
+
+def balance_usd() -> str:
+    eth_usd = float(eth_currency_price_usd())
+    bal_eth = float(balance_eth())
+    response = str(round(eth_usd * bal_eth, 2))
+    return response
+
+
+'''Function returns the average value of the 168
+previous hours (7 days) of hashrate️️'''
+
+
+def hashrate() -> str:
+    link = 'https://api.nanopool.org/v1/eth/avghashratelimited/0x4fcca869dd1dbadd57dfbbcb317410e39a0b0745/Nikaragua/168'
+    response = str(round(requests.get(link).json()['data'], 2))
+    return response
+
+
+'''function returns dictionary arrays which contain in sequence
+values: minute, hour, day, month along with calculated earnings
+from average hash of last 7 days️️'''
+
+
+def calculator() -> list:
+    response = []
+    link = 'https://api.nanopool.org/v1/eth/approximated_earnings/'
+    for i in ['minute', 'hour', 'day', 'week', 'month']:
+        response.append({i: round(requests.get(link + hashrate()).json()['data'][i]['dollars'], 2)})
+    return response
+
+
+@app.route('/')
+def main():
+    conn = sqlite3.connect('nano.db')
+    c = conn.cursor()
+    c.execute("SELECT * FROM nano")
+    page = c.fetchall()
+    return render_template('index.html', my_eth=page[0][1], month=page[1][1], day=page[2][1])
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
---
+++ data.py
@@ -0,0 +1,40 @@
+import sqlite3
+import time
+from app import balance_usd, calculator
+
+
+def create():
+    conn = sqlite3.connect('nano.db')
+    c = conn.cursor()
+    c.execute("""CREATE TABLE nano (name text,money float)""")
+    c.execute("INSERT INTO nano VALUES ('my_eth',1.0)")
+    c.execute("INSERT INTO nano VALUES ('month',1.2)")
+    c.execute("INSERT INTO nano VALUES ('day',1.2)")
+    conn.commit()
+    conn.close()
+
+
+def start():
+    while True:
+        try:
+            start_time = time.time()
+            conn = sqlite3.connect('nano.db')
+            c = conn.cursor()
+            cal = calculator()
+            c.execute('''UPDATE nano SET money = ? WHERE name=?;''', (balance_usd(), "my_eth"))
+            c.execute('''UPDATE nano SET money = ? WHERE name=?;''', (cal[4]['month'], "month"))
+            c.execute('''UPDATE nano SET money = ? WHERE name=?;''', (cal[2]['day'], "day"))
+            conn.commit()
+            c.execute("SELECT * FROM nano")
+            print(c.fetchall(), "%.3f" % (time.time() - start_time), "s")
+        except:
+            print("error")
+
+
+if __name__ == '__main__':
+    try:
+        create()
+        print("im here")
+    except:
+        pass
+    start()
